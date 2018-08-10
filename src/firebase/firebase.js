3056d381b4198e6fc018a2e82616ca8b74b8523e
GeorgeBortov <georgi.bortov@gmail.com>
2018-08-10 22:44:02 +0300

    Add private routes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -12,35 +12,6 @@ const config = {
 firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-
-export { firebase, database as default };
-// // child_removed
-// database.ref('expenses').on('child_removed', (snapshot) => {
-//     console.log(snapshot.key, snapshot.val());
-// });
-
-// // child_changed
-// database.ref('expenses').on('child_changed', (snapshot) => {
-//     console.log(snapshot.key, snapshot.val());
-// });
-
-// // child_added
-// database.ref('expenses').on('child_added', (snapshot) => {
-//     console.log(snapshot.key, snapshot.val());
-// });
-
-// database.ref('expenses')
-// .on('value', (snapshot) => {
-//     const expenses = [];
-//     snapshot.forEach((childSnapshot) => {
-//         expenses.push({
-//             id: childSnapshot.key,
-//             ...childSnapshot.val()
-//         });
-//     });
-
-//     console.log(expenses);
-// }, (e) => {
-//     console.log('Error with data fetching', e);
-// });
+export { firebase, googleAuthProvider, database as default };
